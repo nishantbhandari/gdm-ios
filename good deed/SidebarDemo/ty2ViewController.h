6c29f24c3e516b4a6c842627abdec9a5daa73bbc
nimittheinquister <nimithingu@gmail.com>
2013-11-13 00:33:22 +0530

    Tejas Comit ty2 view

---
+++ good deed/SidebarDemo/ty2ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ty2ViewController.h
+//  Good Deed Marathon
+//
+//  Created by Tejas Hingu on 12/11/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ty2ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIScrollView *ty2ScrollView;
+@property (weak, nonatomic) IBOutlet UIView *ty2View;
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (weak, nonatomic) IBOutlet UITextView *msgTextView;
+
+@end
---
+++ good deed/SidebarDemo/ty2ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ty2ViewController.m
+//  Good Deed Marathon
+//
+//  Created by Tejas Hingu on 12/11/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import "ty2ViewController.h"
+
+@interface ty2ViewController ()
+
+@end
+
+@implementation ty2ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+-(void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    [self.ty2ScrollView layoutIfNeeded];
+    self.ty2ScrollView.contentSize = self.ty2View.bounds.size;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
